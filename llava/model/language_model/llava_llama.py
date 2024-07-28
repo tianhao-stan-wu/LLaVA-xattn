1ddada9636bee91f0102768e06127f8d5564ca44
tianhao-stan-wu <wutianha@usc.edu>
2024-07-28 20:42:31 +0800

    debug

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -699,6 +699,10 @@ class LlavaLlamaForCausalLM(LlamaXAttnForCausalLM, LlavaMetaForCausalLM):
         else:
             inputs_embeds = self.get_model().embed_tokens(inputs)
 
+
+        kwargs['media'] = media
+        kwargs['media_locations'] = media_locations
+
         return super().generate(
             position_ids=position_ids,
             attention_mask=attention_mask,
