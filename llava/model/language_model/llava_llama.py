065c458bcb9fcba8e95a15fe4db0a16bd2e2604b
tianhao-stan-wu <wutianha@usc.edu>
2024-07-29 10:10:09 +0800

    debug

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -37,7 +37,9 @@ from transformers.models.llama.modeling_llama import LlamaDecoderLayer, LlamaRMS
 from transformers.cache_utils import Cache, DynamicCache
 from transformers.modeling_outputs import BaseModelOutputWithPast
 from transformers.utils import logging
-# from transformers.modeling_attn_mask_utils import AttentionMaskConverter
+from transformers.modeling_attn_mask_utils import AttentionMaskConverter
+
+from .cache import StaticCache
 
 
 logger = logging.get_logger(__name__)
@@ -446,8 +448,7 @@ class LlamaXAttnModel(LlamaModel):
         # order to dispatch on Flash Attention 2. This feature is not compatible with static cache, as SDPA will fail
         # to infer the attention mask.
         past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
-        # using_static_cache = isinstance(past_key_values, StaticCache)
-        using_static_cache = False
+        using_static_cache = isinstance(past_key_values, StaticCache)
 
         # When output attentions is True, sdpa implementation's forward method calls the eager implementation's forward
         if self.config._attn_implementation == "sdpa" and not using_static_cache and not output_attentions:
