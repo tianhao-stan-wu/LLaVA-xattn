f61934e84db008e7fa08aa285c7cc95d4dc67a02
tianhao-stan-wu <wutianha@usc.edu>
2024-08-07 17:32:21 +0800

    debug

--- scripts/v1_5/myscripts/pretrain.sh
+++ scripts/v1_5/myscripts/pretrain.sh
@@ -36,8 +36,8 @@ deepspeed llava/train/train_mem.py \
     --deepspeed ./scripts/zero2.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version plain \
-    --data_path ./dataset/LLaVA-Pretrain/blip_laion_cc_sbu_558k.json \
-    --image_folder ./dataset/LLaVA-Pretrain/images \
+    --data_path ../dataset/LLaVA-pretrain/blip_laion_cc_sbu_558k.json \
+    --image_folder ../dataset/LLaVA-pretrain/images \
     --vision_tower openai/clip-vit-large-patch14-336 \
     --pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin \
     --mm_projector_type mlp2x_gelu \
